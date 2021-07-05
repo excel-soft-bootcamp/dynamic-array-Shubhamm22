ef67f22e86fc45ca900eee28dba6f9cee4fa89f3
Shubhamm22 <86050595+Shubhamm22@users.noreply.github.com>
2021-07-05 08:45:45 +0530

    Add files via upload

---
+++ DynamicArrayProgram/DynamicNumberArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicArrayProgram
+{
+     class DynamicNumberArray
+    {
+        int[] buffer;
+        
+        public DynamicNumberArray()
+        {
+            
+            buffer = new int[5]; }
+       
+
+        public   DynamicNumberArray(int initialSize) { buffer = new int[initialSize]; }
+          
+        
+            public void Set(int index, int value)
+            {
+           
+
+                buffer[index] = value;
+            }
+          public   int Get(int index)
+            {
+                return buffer[index];
+            }
+        }
+}
